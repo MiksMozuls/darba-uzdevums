9ccd24476c65188ffb6219641fd61d12604573db
MiksMozuls <miks.mozuls@gmail.com>
2024-02-28 21:26:43 +0200

    Pievienota Mysql datubāze un docker-compose fails

---
+++ DarbaUzdevumaProjekts/Persistance/DataContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace DarbaUzdevumaProjekts.Persistance
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            try {
+                RelationalDatabaseCreator databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if (databaseCreator != null) {
+                    if (!databaseCreator.CanConnect()) {
+                        databaseCreator.Create();
+                    }
+
+                    if (!databaseCreator.HasTables())
+                        databaseCreator.CreateTables();
+                }
+            } 
+                
+            catch (Exception ex) {
+
+                Console.WriteLine(ex.Message); 
+            }
+            
+        }
+    }
+}
--- DarbaUzdevumaProjekts/Program.cs
+++ DarbaUzdevumaProjekts/Program.cs
@@ -1,3 +1,6 @@
+using DarbaUzdevumaProjekts.Persistance;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +9,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_ROOT_PASSWORD");
+var connectionString = $"server={dbHost};port=3306;database={dbName};user=root;password={dbPassword}";
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)); 
+});
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,6 +28,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
